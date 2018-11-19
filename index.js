985bf77f01a8d3c9fe101d1cf88f80927e3a2b97
Olivia Kasten <odkasten@gmail.com>
2018-11-19 00:31:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function deleteFromDriverByKey(driver, key){
   return new_array;
 }
 
-function destructivelyDeleteDriverByKey(driver, key){
+function destructivelyDeleteFromDriverByKey(driver, key){
   delete driver[key];
   return driver;
 }
